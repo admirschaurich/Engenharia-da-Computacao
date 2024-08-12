3c24e667dfac05820a30f2c32e4e52aa6242703d
Admir Schaurich <admirsch@gmail.com>
2024-08-12 20:40:57 -0300

    Adicionado o exercicio prático PR2 de Desenvolvimento Dinamico

---
+++ Desenvolvimento Dinamico/PR2-Instrucoes/PR2-Intrucoes.js
@@ -0,0 +1,42 @@
+/*Escreva um código em que, após o cliente solicitar a escolha da bebida, 
+a opção ‘switch’ deve avaliar a opção selecionada e atribuir o valor correspondente à variável ‘valor’. 
+Caso o cliente escolha uma opção inválida, uma mensagem informativa é exibida, 
+indicando que a escolha deve ser feita entre café, leite ou chá. Por fim, 
+se uma opção válida for selecionada, exibimos uma mensagem personalizada informando 
+o nome da bebida escolhida e o valor a ser pago, formatado com duas casas decimais.*/
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true
+});
+
+rl.question('Digite a opção desejada: café, leite ou chá: ', (option) => {
+    let value = 0;
+
+    console.log(option.trim());
+
+    switch (option.trim()) {
+        case 'café':
+            value = 7;
+            break;
+
+        case 'leite':
+            value = 3.50;
+            break;
+
+        case 'chá':
+            value = 2.10;
+            break;
+
+        default:
+            value = 0.00;
+            console.log(`Opção não encontrada. Você deve selecionar uma das opções: café, leite ou chá.`);
+            rl.close();
+            return;
+    }
+
+    console.log(`Você selecionou a bebida ${option} e custa ${value.toFixed(2)}`);
+    rl.close();
+});
